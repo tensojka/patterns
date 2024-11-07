bca48082a456b6767e6d398a8c4658cf34dede16
Ondřej Sojka <ondrej.sojka@gmail.com>
2024-11-07 16:50:11 +0100

    Update nix env to use python 3.8 for wikiextractor

--- shell.nix
+++ shell.nix
@@ -2,20 +2,20 @@
 # Note: wikiextractor requires Python 3.9
 
 let
-  python39 = pkgs.python39;
+  python38 = pkgs.python38;
   python312 = pkgs.python312;
 
-  wikiextractor = python39.pkgs.buildPythonPackage rec {
+  wikiextractor = python38.pkgs.buildPythonPackage rec {
     pname = "wikiextractor";
     version = "3.0.6";
-    src = python39.pkgs.fetchPypi {
+    src = python38.pkgs.fetchPypi {
       inherit pname version;
       sha256 = "cEH/V4hMFkCem5EczQEGwhi4jLzqu4bdFENjevqChN4=";
     };
     doCheck = false;
   };
 
-  python39WithPackages = python39.withPackages (ps: [ wikiextractor ]);
+  python38WithPackages = python38.withPackages (ps: [ wikiextractor ]);
   python312WithPackages = python312.withPackages (ps: with ps; [
     regex 
     numpy
@@ -29,18 +29,18 @@ in pkgs.mkShell {
     rustc
     cargo
     espeak
-    python39WithPackages
+    python38WithPackages
     python312WithPackages
     texliveWithPackages
     libiconv
   ];
 
   shellHook = ''
-    alias python3.8=${python39WithPackages}/bin/python
-    alias pip3.8=${python39WithPackages}/bin/pip
+    alias python3.8=${python38WithPackages}/bin/python
+    alias pip3.8=${python38WithPackages}/bin/pip
     alias python3.12=${python312WithPackages}/bin/python
     alias pip3.12=${python312WithPackages}/bin/pip
-    alias wikiextractor=${python39WithPackages}/bin/wikiextractor
+    alias wikiextractor=${python38WithPackages}/bin/wikiextractor
 
     # Set Python 3.12 as default
     alias python=${python312WithPackages}/bin/python
